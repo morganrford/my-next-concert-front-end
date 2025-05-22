57b71f6b5d242519fd971cec39e9feef0cc5ce72
Coba Paz <cobajayy@gmail.com>
2025-05-22 15:52:53 -0600

    created concertForm, bandsForm, handlers in app.jsx

--- src/components/BandsForm/BandsForm.jsx
+++ src/components/BandsForm/BandsForm.jsx
@@ -1,9 +1,59 @@
-const BandsForm = () => {
-    return (
-        <div>
-            <h1>Bands Form</h1>
-        </div>
-    )
+const BandsForm = (props) => {
+  const initialState = {
+    name: "",
+    genre: "",
+    members: "",
+  };
+  // formData state to control the form.
+  const [formData, setFormData] = useState(
+    props.selected ? props.selected : initialState
+  );
+
+  // handleChange function to update formData state.
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+  const handleSubmit = (evt, bandId) => {
+    evt.preventDefault();
+    if (props.selected) {
+      props.handleUpdateBand (formData, bandId);
+    } else {
+      props.handleAddBand (formData);
+    }
+  };
+  // And finally, the form itself.
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name"> Name </label>
+        <input
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="genre"> Genre </label>
+        <input
+          id="genre"
+          name="genre"
+          value={formData.genre}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="members"> Members </label>
+        <input
+          id="members"
+          name="members"
+          value={formData.members}
+          onChange={handleChange}
+        />
+        <button type="submit">
+          {props.selected ? "Update Band" : "Add New Band"}
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default BandsForm;
--- src/components/ConcertForm/ConcertForm.jsx
+++ src/components/ConcertForm/ConcertForm.jsx
@@ -1,9 +1,115 @@
-const ConcertForm = () => {
-    return (
+import { useState } from "react";
+const ConcertForm = (props) => {
+  const initialState = {
+    venueName: "",
+    venueAddress: "",
+    date: "",
+    time: "",
+    price: "",
+    promoter: "",
+    bands: "",
+  };
+
+  const [formData, setFormData] = useState(
+    props.selected ? props.selected : initialState
+  );
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt, concertId) => {
+    evt.prevent.default();
+    if (props.selected) {
+      props.handleUpdateConcert(formData, concertId);
+    } else {
+      props.handleAddConcert(formData);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Concert Form</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h1>Concert Form</h1>
+          <label htmlFor="venue-name">Venue Name:</label>
+          <input
+            id="venue-name"
+            name="venue-name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
-    )
-}
+        <div>
+          <label htmlFor="venue-address">Venue Address:</label>
+          <input
+            id="venue-address"
+            name="venue-address"
+            value={formData.venueAddress}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="date">Date:</label>
+          <input
+            id="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="time">Time:</label>
+          <input
+            id="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="price">Price:</label>
+          <input
+            id="price"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="promoter">Promoter:</label>
+          <input
+            id="promoter"
+            name="promoter"
+            value={formData.promoter}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor="bands">Bands:</label>
+          <input
+            id="bands"
+            name="bands"
+            value={formData.bands}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">{props.selected ? "Update Concert" : "Add New Concert"}</button>
+      </form>
+    </div>
+  );
+};
 
 export default ConcertForm;
